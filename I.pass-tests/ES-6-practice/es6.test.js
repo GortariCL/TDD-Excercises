ebf21f51f1095db2516589f5968cb74ee48679f5
Fernando Gortari <fgortari@mobdev.cl>
2022-06-08 18:49:23 -0400

    Faltan ejercicios 10 y 11

--- I.pass-tests/ES-6-practice/es6.test.js
+++ I.pass-tests/ES-6-practice/es6.test.js
@@ -3,41 +3,43 @@ const {
   defaultParameter,
   combineArrays,
   destructuring,
-  templateString
-} = require("./index");
+  templateString,
+} = require("./es6.js");
 
-test("function arrow is instance of a function", () => {
-  expect(arrow).toBeInstanceOf(Function);
+describe("given the functions", () => {
+  it("function arrow is instance of a function", () => {
+    expect(arrow).toBeInstanceOf(Function);
+  });
+
+  it("function arrow returns a string of es6 is awesome", () => {
+    expect(arrow()).toEqual("es6 is awesome!");
+  });
+
+  it("defaultParameter function should return sam if no name argument passed in", () => {
+    expect(defaultParameter()).toEqual("sam");
+  });
+
+  it("defaultParameter function should return name if name argument is passed in", () => {
+    expect(defaultParameter("samatar")).toEqual("samatar");
+  });
+
+  it("combineArrays function should combine arr1 and arr2", () => {
+    expect(combineArrays(arr1, arr2)).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it("destructuring function should return cyf property of object", () => {
+    expect(destructuring(testObj)).toEqual("awesome");
+  });
+
+  it("templateString should return a string with the sum of parameter a and b", () => {
+    expect(templateString(1, 2)).toEqual("The sum is equal to 3");
+  });
+
+  const arr1 = [1, 2, 3];
+  const arr2 = [4, 5, 6];
+  const testObj = {
+    cyf: "awesome",
+    test: "100%",
+    learning: "always",
+  };
 });
-
-test("function arrow returns a string of es6 is awesome", () => {
-  expect(arrow()).toEqual("es6 is awesome!");
-});
-
-test("defaultParameter function should return sam if no name argument passed in", () => {
-  expect(defaultParameter()).toEqual("sam");
-});
-
-test("defaultParameter function should return name if name argument is passed in", () => {
-  expect(defaultParameter("samatar")).toEqual("samatar");
-});
-
-test("combineArrays function should combine arr1 and arr2", () => {
-  expect(combineArrays(arr1, arr2)).toEqual([1, 2, 3, 4, 5, 6]);
-});
-
-test("destructuring function should return cyf property of object", () => {
-  expect(destructuring(testObj)).toEqual("awesome");
-});
-
-test("templateString should return a string with the sum of parameter a and b", () => {
-  expect(templateString(1, 2)).toEqual("The sum is equal to 3");
-});
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-const testObj = {
-  cyf: "awesome",
-  test: "100%",
-  learning: "always"
-};
